a5f15b8045b626468a6ab231eb56af509c409709
Kanwar-sikandar-ali <sikandar.ali@enabill.com>
2023-09-10 02:20:23 +0500

    update http to https

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const app = require("express")();
-const server = require("http").createServer(app);
+const server = require("https").createServer(app);
 const cors = require("cors");
 
 const io = require("socket.io")(server, {
-	cors: {
-		origin: "*",
-		methods: ["GET", "POST"]
-	}
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
 });
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-	res.send("Running.");
+    res.send("Running.");
 });
 
 io.on("connection", (socket) => {
-    socket.emit('me', socket.id);
+    socket.emit('me', socket.id);   
 
 
     // we are not using this one 
